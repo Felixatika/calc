fbb3b181b2552a8601afa53e73f15252ce0cfd9c
Felix <felixatika@gmail.com>
2020-05-06 06:49:29 -0400

    add var result in each function

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,29 @@ var add = function (number1, number2) {
 };
 var number1 = parseInt(prompt("Enter a number: "));
 var number2 = parseInt(prompt("Enter another number: "));
-alert(add(number1, number2));
+var result = add(number1, number2);
+alert(result);
 
 var subtract = function (number1, number2) {
   return number1 - number2;
 };
 var number1 = parseInt(prompt("Enter a number: "));
 var number2 = parseInt(prompt("Enter another number: "));
-alert(subtract(number1, number2));
+var result = subtract(number1, number2);
+alert(result);
 
 var multiply = function (number1, number2) {
   return number1 * number2;
 };
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-alert(multiply(number1, number2));
+var result = multiply(number1, number2);
+alert(result);
 
 var divide = function (number1, number2) {
-  return number1 - number2;
+  return number1 / number2;
 };
 var number1 = parseInt(prompt("Enter a number: "));
 var number2 = parseInt(prompt("Enter another number: "));
-alert(divide(number1, number2));
+var result = divide(number1, number2);
+alert(result);
